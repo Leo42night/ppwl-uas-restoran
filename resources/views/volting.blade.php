360315e462d31c7620dadb84b13ba2037078dbaf
Leo42night <109854127+Leo42night@users.noreply.github.com>
2024-05-29 02:08:11 +0700

    persiapan template

---
+++ resources/views/volting.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app-volt')
+
+@section('css')
+@endsection
+@section('content')
+test
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hh', function() {
+    return "haha";
+    // return view('');
+});
+
+Route::get('index',function() {
+    return view('index-material');
+});
+
+Route::get('halaman',function(){
+    return view('volting');
+});
